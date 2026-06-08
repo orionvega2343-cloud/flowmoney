68b51bc1a00d033b1a7fce8c88977fc437836bf2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-08 22:55:40 +0300

    feat: add transaction model

--- api/internal/models/transaction.go
+++ api/internal/models/transaction.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Transaction struct {
+	Id         int       `json:"id" db:"id"`
+	UserId     int       `json:"user_id" db:"user_id"`
+	Amount     float64   `json:"amount" db:"amount"`
+	Type       string    `json:"type" db:"type"`
+	Date       time.Time `json:"date" db:"date"`
+	CategoryId int       `json:"category_id" db:"category_id"`
+}
