bf749b9eb612d1df235f11019900372b0afa6f94
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-09 18:17:36 +0300

    feat: add category repo

--- api/internal/repository/categoryRepo.go
+++ api/internal/repository/categoryRepo.go
@@ -1,1 +1,43 @@
 package repository
+
+import (
+	"flowmoney/api/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type CategoryRepo struct {
+	db *sqlx.DB
+}
+
+func NewCategoryRepo(db *sqlx.DB) *CategoryRepo {
+	return &CategoryRepo{db: db}
+}
+
+func (r *CategoryRepo) CreateCategory(c models.Category) (models.Category, error) {
+	err := r.db.QueryRow(`INSERT INTO categories(title,user_id) VALUES($1,$2) RETURNING id`, c.Title, c.UserId).Scan(&c.Id)
+	if err != nil {
+		return models.Category{}, err
+	}
+	return c, nil
+
+}
+
+func (r *CategoryRepo) GetCategoryById(id int) (models.Category, error) {
+	var c models.Category
+	err := r.db.Get(&c, "SELECT * FROM categories WHERE id=$1", id)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
+}
+
+func (r *CategoryRepo) GetByUserId(id int) ([]models.Category, error) {
+	var c []models.Category
+	err := r.db.Select(&c, "SELECT * FROM categories WHERE user_id=$1", id)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
+
+}
