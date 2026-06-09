e956d3316421e7ac9ae98045deefc5b44f80b7da
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-10 01:48:35 +0300

    feat: add connect db

--- api/internal/db/db.go
+++ api/internal/db/db.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"flowmoney/api/internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func ConnectDb(cfg config.DB) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf(" host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, cfg.Pass, cfg.Name, cfg.SslMode)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
