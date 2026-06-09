8c32a84a58946b5393281df6bd502a88ebca3df2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-09 21:01:20 +0300

    feat: add transaction repo

--- api/internal/repository/transactionRepo.go
+++ api/internal/repository/transactionRepo.go
@@ -1,1 +1,41 @@
 package repository
+
+import (
+	"flowmoney/api/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type TransactionRepo struct {
+	db *sqlx.DB
+}
+
+func NewTransactionRepo(db *sqlx.DB) *TransactionRepo {
+	return &TransactionRepo{db: db}
+}
+
+func (r *TransactionRepo) CreateTransaction(tr models.Transaction) (models.Transaction, error) {
+	err := r.db.QueryRow(`INSERT INTO transactions(user_id,amount,type,date,category_id) VALUES($1,$2,$3,$4,$5) RETURNING id`, tr.UserId, tr.Amount, tr.Type, tr.Date, tr.CategoryId).Scan(&tr.Id)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+	return tr, nil
+}
+
+func (r *TransactionRepo) GetTransactionById(id int) (models.Transaction, error) {
+	var tr models.Transaction
+	err := r.db.Get(&tr, "SELECT * FROM transactions WHERE id = $1", id)
+	if err != nil {
+		return models.Transaction{}, err
+	}
+	return tr, nil
+}
+
+func (r *TransactionRepo) GetTransactionByUserId(userId int) ([]models.Transaction, error) {
+	var tr []models.Transaction
+	err := r.db.Select(&tr, "SELECT * FROM transactions WHERE user_id = $1", userId)
+	if err != nil {
+		return []models.Transaction{}, err
+	}
+	return tr, nil
+}
