96690393b1505e81ca09b44c6bfb61fc9f66e51a
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-14 00:53:22 +0300

    feat: Add middlewares

--- api/internal/middlewares/auth.go
+++ api/internal/middlewares/auth.go
@@ -1,1 +1,43 @@
 package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// Структура параметров
+type Claims struct {
+	UserId int    `json:"user_id"`
+	Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func AuthMiddleware(next http.Handler, secret string) http.Handler {
+	//Замыкание
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		tokenStr := r.Header.Get("Authorization") //Получаем токен по тегу
+		//Валидируем токен
+		if tokenStr == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		trimmed := strings.TrimPrefix(tokenStr, "Bearer ") //Обрезаем префикс
+		//Генерируем токен
+		token, err := jwt.ParseWithClaims(trimmed, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+		//Проверяем на валидность
+		if err != nil || token == nil || !token.Valid {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		//Вызываем хендлер
+		ctx := context.WithValue(r.Context(), "user", token.Claims.(*Claims))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
--- api/internal/middlewares/logger.go
+++ api/internal/middlewares/logger.go
@@ -1,1 +1,35 @@
 package middlewares
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type responseWriterWrapper struct {
+	ResponseWriter http.ResponseWriter
+	statusCode     int
+}
+
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrapped := &responseWriterWrapper{ResponseWriter: w, statusCode: http.StatusOK}
+		start := time.Now()
+		next.ServeHTTP(wrapped, r)
+		log.Println(r.Method, r.URL.Path, wrapped.statusCode, time.Since(start))
+	})
+
+}
+
+func (w *responseWriterWrapper) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *responseWriterWrapper) Header() http.Header {
+	return w.ResponseWriter.Header()
+}
+
+func (w *responseWriterWrapper) Write(b []byte) (int, error) {
+	return w.ResponseWriter.Write(b)
+}
