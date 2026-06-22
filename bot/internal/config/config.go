158a70ffb0a9f41d1e9a4ade4cc0e3baa40a5135
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-22 18:27:12 +0300

    feat: Add bot config

--- bot/internal/config/config.go
+++ bot/internal/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ApiUrl string `yaml:"api_url" env:"API_URL"`
+	Token  string `env:"BOT_TOKEN"`
+}
+
+func MustLoad() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var cfg = Config{}
+
+	err = cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal("Error loading config")
+	}
+	return &cfg
+}
